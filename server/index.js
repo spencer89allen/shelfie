d8e7a709aaf8803723eb280ffeaa810be8048018
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-02-27 19:03:03 -0700

    step 1 complete

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var massive = require('massive')
 
-require('dotenv').config();
+require('dotenv').config({ path:__dirname + '/.env'});
+// ^ figure out what this line does
 
 var cntrl = require('./controller')
 
@@ -10,7 +11,10 @@ var app = express();
 
 massive( process.env.CONNECTION_STRING ).then( dbInstance => {
     app.set('db', dbInstance)
+    // ^ still need to create our table
     console.log(`The database is connected`)
+}).catch(e => {
+    console.error(e);
 });
 
 app.use(bodyParser.json());
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
 
 
+
 class Form extends Component {
+    state = {
+        image: '',
+        name: '',
+        price: '',
+        product: [],
+    };
+
+    handleInput(name, value) {
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    handleAddNewProduct () {
+        const { image, name, price } = this.state;
+        var newProduct = this.state.product
+        newProduct.push({ image, name, price });
+        this.setState({
+            product: newProduct,
+            image: '',
+            name: '',
+            price: '',
+        });
+    }
+
+    handleCancel () {
+        this.setState({
+            image: '',
+            name: '',
+            price: '', 
+        })
+
+    }
 
     render() {
         return (
-            <div>
+            <div >
                 <p>Form</p>
+                <p>
+                    <input  placeholder=' Image URL' 
+                            value={this.state.image}
+                            onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                            name="image"
+                            />
+                </p>
+                <p>
+                    <input  placeholder=' Product Name' 
+                            value={this.state.name}
+                            onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                            name="name"
+                            />
+                </p>
+                <p>
+                    <input  placeholder=' Price' 
+                            value={this.state.price}
+                            onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                            name="price"
+                            />
+                </p>
+                <button onClick={() => this.handleAddNewProduct()}>Add to Inventory</button>
+                <button onClick={() => this.handleCancel()}>Cancel</button>
             </div>
         )
     }
