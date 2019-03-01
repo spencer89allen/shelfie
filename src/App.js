9b8354c3badece511b3af8f731082c776fe45852
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-02-28 17:50:30 -0700

    part 1 step 2 completed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,28 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import Form from './Components/Form/Form';
 
 class App extends Component {
+ 
+  state = {
+    inventory: [ 
+      {
+        image:'https://http.cat/100',
+        name: 'Cat 1',
+        price: 15,
+      }, 
+      {
+        image:'https://http.cat/404',
+        name: 'Cat 2',
+        price: 20
+      } 
+    ],
+  }
+
+  
   render() {
     return (
       <div className="App">
         <Header/>
-        <Dashboard/>
+        <Dashboard list={this.state.inventory}/>
         <Form/>
       </div>
     );
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,11 +5,24 @@ import React, { Component } from 'react';
 import Product from '../Product/Product';
 
 class Dashboard extends Component {
-
+    
     render() {
+        const display = this.props.list.map((element, id) => {
+            return (
+                <div key={id}>
+                    <p></p>
+                    {element.name}
+                    <p></p>
+                    {element.image}
+                    <p></p>
+                    {element.price}
+                </div>
+            )
+        })
         return (
             <div>
                 <p>Dashboard</p>
+                {display}
                 <Product/>
             </div>
         )
